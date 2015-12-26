d0b5b5adee0262b222100a001c69dde6b810f8b4
S.Prymak <sprymak@metacorus.com>
2015-12-27 00:34:42 +0200

    Fixed several bugs in wigs.py

--- application/wsgi.py
+++ application/wsgi.py
@@ -1,28 +1,21 @@
-"""
-WSGI config for metacorus-dev project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
 import os
-from twod.wsgi.handler import DjangoApplication
-from conf.base import *
-
-os.environ['DJANGO_SETTINGS_MODULE'] = "settings"
+from django.core.handlers.wsgi import WSGIHandler as _WSGIHandler
+from django.core.wsgi import get_wsgi_application
 
 
-class Application(DjangoApplication):
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+
+class WSGIHandler(_WSGIHandler):
+    def __init__(self, application, base_dir=None):
+        self.application = application
+        super(WSGIHandler, self).__init__()
+
     def __call__(self, environ, start_response):
-        if 'PYTHON_EGG_CACHE' in environ:
-            os.environ['PYTHON_EGG_CACHE'] = environ['PYTHON_EGG_CACHE']
-        else:
-            os.environ['PYTHON_EGG_CACHE'] = PYTHON_EGG_CACHE
         os.environ['wsgi.url_scheme'] = environ.get('wsgi.url_scheme', '')
         os.environ['HTTP_HOST'] = environ.get('HTTP_HOST', '')
         os.environ['SERVER_PORT'] = environ.get('SERVER_PORT', '')
-        return super(Application, self).__call__(environ, start_response)
+        return self.application(environ, start_response)
 
 
-application = Application()
+application = WSGIHandler(get_wsgi_application())
