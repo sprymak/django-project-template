aa9cb309d835a7d95eda14aa3362c80597869335
S.Prymak <sprymak@metacorus.com>
2015-12-27 16:05:58 +0200

    Fix template assets name

--- application/apps/project_name/assets.py
+++ application/apps/project_name/assets.py
@@ -1,23 +1,23 @@
 from django_assets import Bundle, register
 
 
-register('project_name_css', Bundle(
+register('{{ project_name }}_css', Bundle(
     Bundle(
-        "project_name/styles/project_name.scss",
+        "{{ project_name }}/styles/{{ project_name }}.scss",
         filters="pyscss",
     ),
     filters='cssrewrite,cssmin',
-    output='_/project_name.css'
+    output='_/{{ project_name }}.css'
 ))
 
-register('project_name_js', Bundle(
+register('{{ project_name }}_js', Bundle(
     Bundle(
-        "project_name/scripts/project_name.coffee",
+        "{{ project_name }}/scripts/{{ project_name }}.coffee",
         filters='coffeescript,jsmin',
     ),
     Bundle(
-        "project_name/scripts/project_name.js",
+        "{{ project_name }}/scripts/{{ project_name }}.js",
         filters='jsmin'
     ),
-    output='_/project_name.js'
+    output='_/{{ project_name }}.js'
 ))
